adcadc523cd00608d1aef9f1ad8a974418b329f4
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-07-09 11:30:55 -0500

    typos lol

--- models/__init__.py
+++ models/__init__.py
@@ -6,7 +6,7 @@ HBNB_TYPE_STORAGE = getenv("HBNB_TYPE_STORAGE")
 
 
 if HBNB_TYPE_STORAGE == "db":
-    from models.engine.dbstorage import DBStorage
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
